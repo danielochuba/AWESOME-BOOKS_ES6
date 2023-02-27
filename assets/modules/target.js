892c2ea41f3eca7e1ec220c430d60bbeef5a8ab2
Daniel Ochuba <danielochuba78@gmail.com>
2023-02-28 00:43:13 +0100

    add pages and functionality

---
+++ assets/modules/target.js
@@ -0,0 +1,6 @@
+export  const listSection = document.querySelector('.list-section');
+export  const addNewSection = document.querySelector('.add-new-section');
+export  const contactSection = document.querySelector('.contact-section');
+export  const listLink = document.querySelector('.link1');
+export  const addNewLink = document.querySelector('.link2');
+export  const contactLink = document.querySelector('.link3');
